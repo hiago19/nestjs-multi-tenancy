07725653c760604b29ff8f816e676712b05cb003
Bruno Hiago <bhiago99@gmail.com>
2024-06-19 18:30:33 -0300

    first commit

---
+++ src/auth/users/user-roles.ts
@@ -0,0 +1,4 @@
+export enum UserRoles {
+    PARTNER = 'PARTINER',
+    USER = 'USER',
+}
---
+++ src/tenant/tenant.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, Scope } from '@nestjs/common';
+import { Partner } from '@prisma/client';
+
+/*
+Service levels:
+1 - shared service | singleton
+2 - scope service | request service
+3 - transient service
+*/
+
+@Injectable({
+    scope: Scope.REQUEST,
+}) 
+export class TenantService {
+    private tenant: Partner;
+
+    setTenant(tenant: Partner) {
+        this.tenant = tenant;
+    }
+
+    getTenant() {
+        return this.tenant;
+    }
+}
